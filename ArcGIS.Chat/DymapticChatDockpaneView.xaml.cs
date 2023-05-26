e8e881acbd836efecdbd7db4783201f40655ab08
Ande Bell <ande.bell@dymaptic.com>
2023-05-26 17:09:12 -0600

    Adding framework fro ArcGIS plugin

---
+++ ArcGIS.Chat/DymapticChatDockpaneView.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+
+namespace dymaptic.chat.arcgis
+{
+    /// <summary>
+    /// Interaction logic for DymapticChatDockpaneView.xaml
+    /// </summary>
+    public partial class DymapticChatDockpaneView : UserControl
+    {
+        public DymapticChatDockpaneView()
+        {
+            InitializeComponent();
+        }
+    }
+}
