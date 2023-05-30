8adcfc3de8f5af24de4b7350396f1923df871902
Tim Rawson <rawson.t@gmail.com>
2023-05-30 10:59:11 -0700

    separate addition to existing repo

---
+++ dy-chat/DyChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Dy_Chat
+{
+    public class DyChatHub : Hub
+    {
+        public const string HubUrl = "/dy-chat-hub";
+        public async Task Broadcast(string username, string message)
+        {
+            Console.WriteLine($"Broadcast initiated for {username}");
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Client connected: {Context.ConnectionId}");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception ex)
+        {
+            Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
+            return base.OnDisconnectedAsync(ex);
+        }
+    }
+}
---
+++ dy-chat/Program.cs
@@ -0,0 +1,44 @@
+using dy_chat.Business;
+using dy_chat.Data;
+using Dy_Chat;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+
+namespace dy_chat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<DyChatMessageHandler>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+            app.MapHub<DyChatHub>(DyChatHub.HubUrl);
+
+            app.Run();
+        }
+    }
+}
