2f667432451d71260e4500f6fe43d19649a385be
Tim Rawson <rawson.t@gmail.com>
2023-05-30 08:49:29 -0700

    file transfer complete - does not build - need to get arcgis pro add-in access

---
+++ ArcGIS.Chat/Data/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace dy_chat.Data;
+
+public class ChatMessage
+{
+    public ChatMessage(string username, string body, bool isMine)
+    {
+        Username = username;
+        Body = body;
+        IsMine = isMine;
+    }
+
+    public string Username { get; set; }
+    public string Body { get; set; }
+    public bool IsMine { get; set; }
+
+    public bool IsNotice => Body?.StartsWith("[Notice]") ?? false;
+    // ask about the CSS down here, assume that it is user specified
+    public string CSS => IsMine ? "sent" : "received";
+}
