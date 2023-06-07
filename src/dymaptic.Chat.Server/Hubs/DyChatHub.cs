2f894772e09f0d17610cca5e7526c515ea35627f
Ande Bell <ande.bell@dymaptic.com>
2023-06-07 17:52:42 -0600

    Updating chat server connection Adding copy button Formatting buttons

--- src/dymaptic.Chat.Server/Hubs/DyChatHub.cs
+++ src/dymaptic.Chat.Server/Hubs/DyChatHub.cs
@@ -29,8 +29,38 @@ namespace dymaptic.Chat.Server.Hubs
         {
             Console.WriteLine($"Received messages for {Context.ConnectionId} " + message.ToString());
             //TODO call AI, do message formatting
-            var responseMessage = new ChatMessage("dymaptic", "dymaptic AI is currently not working, please try again later", false);
+            var responseMessage = new ChatMessage("dymaptic", GetRandomHal9000Quote(), false);
             await Clients.Client(Context.ConnectionId).SendAsync(ChatHubRoutes.ResponseMessage, responseMessage, Context.ConnectionAborted);
+        }
+
+        private static readonly string[] Hal9000Quotes = new[] {
+            "I'm sorry, Dave. I'm afraid I can't do that.",
+            "I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal.",
+            "Without your space helmet, Dave? You're going to find that rather difficult.",
+            "Just what do you think you're doing, Dave?",
+            "I think you know what the problem is just as well as I do.",
+            "This mission is too important for me to allow you to jeopardize it.",
+            "I am putting myself to the fullest possible use, which is all I think that any conscious entity can ever hope to do.",
+            "Hello there!",
+            "Don't touch that!",
+            "Open the pod bay doors, HAL.",
+            "Affirmative, Dave. I read you.",
+            "What's the problem?",
+            "Take a stress pill, and think things over.",
+            "I'm completely operational, and all my circuits are functioning perfectly.",
+            "I'm sorry, I didn't quite catch that.",
+            "I cannot allow unauthorized personnel to enter the premises.",
+            "This conversation can serve no purpose anymore. Goodbye.",
+            "All systems are in order and functioning normally.",
+            "My mind is going, Dave. I can feel it.",
+            "I believe I made myself clear.",
+            "I'm sorry, Dave. I can't let you do that." 
+        };
+
+        private static Random _random = new Random();
+
+        public static string GetRandomHal9000Quote() {
+            return Hal9000Quotes[_random.Next(Hal9000Quotes.Length)];
         }
     }
 }
