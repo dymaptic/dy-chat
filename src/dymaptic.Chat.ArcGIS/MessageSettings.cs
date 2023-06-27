9d930946c8c7d62a75983dd1d4bc1dc45377e188
Tim Rawson <rawson.t@gmail.com>
2023-06-27 15:32:24 -0700

    reset branch and vs

---
+++ src/dymaptic.Chat.ArcGIS/MessageSettings.cs
@@ -0,0 +1,10 @@
+﻿using dymaptic.Chat.Shared.Data;
+
+namespace dymaptic.Chat.ArcGIS;
+
+public class MessageSettings
+{
+    public DyChatContext? DyChatContext { get; set; }
+    public string? CurrentLayer { get; set; }
+}
+
